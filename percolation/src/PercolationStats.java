de66846bb6f40014b34fb62984db71dcfac0de9c
Rafael Paz Silva <rafaelpaz@petrobras.com.br>
2018-01-24 09:20:46 -0200

    Now it is OK

--- percolation/src/PercolationStats.java
+++ percolation/src/PercolationStats.java
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-
 import java.util.*;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class PercolationStats {
@@ -64,69 +63,13 @@ public class PercolationStats {
 	}
 
 	public static void main(String[] args) {
-		
-		PercolationStats stats = new PercolationStats(200, 100);		
+		assertTrue((args.length == 2), "Error: Please inform 2 parameters: size N for a grid with NxN and number of trials");
+		assertTrue((Integer.parseInt(args[0]) > 0 && Integer.parseInt(args[1]) > 0), "Error: Parameters must be greater than zero");
+				
+		PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));		
 		System.out.println("mean					= " + stats.mean() );
 		System.out.println("stddev					= " + stats.stddev() );
 		System.out.println("95% confidence interval	= [" + (stats.mean() - stats.stddev()) + ", " + (stats.mean() + stats.stddev()) + "]");
 		
 	}
 }
-=======
-
-import java.util.*;
-import edu.princeton.cs.algs4.StdRandom;
-
-
-public class PercolationStats {
-	
-	int size;
-	List<Double> sitesToPerc;
-	Percolation perc;
-	
-	public PercolationStats(int n, int trials) {
-		int row, col;
-		this.size = n;		
-		this.sitesToPerc = new ArrayList<Double>();
-		this.perc = new Percolation(n);
-		
-		for (int i = 0; i < trials; i++) {
-			sitesToPerc.add(0.0);
-			
-			while (!perc.percolates()) {
-				row = StdRandom.uniform(this.size)+1;
-				col = StdRandom.uniform(this.size)+1;				
-				perc.open(row, col);				
-				sitesToPerc.set(i, sitesToPerc.get(i).doubleValue() + 1);				
-			}
-			
-			sitesToPerc.set(i, sitesToPerc.get(i).doubleValue() / trials);	
-			perc.reset();
-		}
-	}
-	
-	public double mean() {
-		//return sitesToPerc.get(0)/sitesToPerc.size();
-		
-		return sitesToPerc
-				.stream()
-				.mapToDouble(e -> e.intValue())
-	            .reduce(0, (sum,x) -> (sum + x)) / sitesToPerc.size();
-	          
-	}
-	public double stddev() {
-		return 0;
-	}
-	public double confidenceLo() {
-		return 0;
-	}
-	public double confidenceHi() {
-		return 0;
-	}
-
-	public static void main(String[] args) {
-		PercolationStats stats = new PercolationStats(200, 100);		
-		System.out.println("Threshold: " + stats.mean() );
-	}
-}
->>>>>>> 093657dea6420a6fc3ffe42e51947c8389bc64e1
